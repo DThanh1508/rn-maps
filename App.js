0b98ee4536a9f9cbbe22623b54460becb2a42589
DThanh1508 <athanh2002kt@gmail.com>
2022-11-04 17:01:21 +0700

    Display simple map

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Map from './src/containers/Map'
+
+const App = () => {
+  return (
+      <Map/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
