925b9b2f46453faa5e5b00fff3d476ac22b9d2fe
DThanh1508 <athanh2002kt@gmail.com>
2022-11-04 17:11:26 +0700

    Uploaded image simple map

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -11,21 +11,17 @@ const Map = () => {
     })
     return (
         <View style={styles.container}>
-            {/*Render our MapView*/}~
             <MapView
                 style={styles.map}
-                //specify our coordinates.
                 initialRegion={{
                     latitude: 37.78825,
                     longitude: -122.4324,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-                //onRegionChangeComplete runs when the user stops dragging MapView
                 onRegionChangeComplete={(region) => setRegion(region)}
 
             />
-            {/*Display user's current region:*/}
             <Text style={styles.text}>Current latitude: {region.latitude}</Text>
             <Text style={styles.text}>Current longitude: {region.longitude}</Text>
         </View>
